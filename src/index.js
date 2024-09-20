9f443b68bd152bacf4e827b9bcf22d3eeeaf9282
Adithya kamath <kamathadithya12@gmail.com>
2024-09-20 13:17:51 +0530

    req handlers demo

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hi", (req, res) => {
-  res.send("response from server");
-});
+app.use(
+  "/test",
+  (req, res, next) => {
+    console.log("1");
+    // res.send("1");
+    next();
+  },
+  (req, res) => {
+    console.log("2");
+    next();
+    // res.send("2");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is succesfully running on port 3000");
